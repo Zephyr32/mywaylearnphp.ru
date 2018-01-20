eb45b5c21bc536373d5cf2ddcbeda716affc3185
AlxSem <alecseysemenenko@gmail.com>
2018-01-20 09:42:39 +0200

    learning if else

--- www/index.php
+++ www/index.php
@@ -8,6 +8,15 @@
 </body>
 </html>
 <?
-echo "My first php func";
-
+echo "My first","</br>"," php func";
+echo "Был дедуля","</br>","звали деда Моросюлей","</br>";
+echo "У него был конь","</br>","звали его Понь","</br>";
+$first = 100;
+$second = 330;
+$answer = $first+$second;
+echo $first + $second;
+if ($answer==400)
+{
+	echo 'HIIIII! answer == 400!!';
+}
 ?>
